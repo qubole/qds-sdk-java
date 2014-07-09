4a6e465808ca7c3dca3db26ba315c9145d236378
randgalt <jordan@jordanzimmerman.com>
2014-07-09 17:26:50 -0500

    Completed testing and doc of the result latch

--- src/main/java/com/qubole/qds/sdk/java/client/ResultLatch.java
+++ src/main/java/com/qubole/qds/sdk/java/client/ResultLatch.java
@@ -7,38 +7,87 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * A utility for polling for a command result. Allows for blocking or
+ * callback on a running query
+ */
 public class ResultLatch
 {
     private final QdsClient client;
     private final String queryId;
+    private final AtomicLong pollMs = new AtomicLong(DEFAULT_POLL_MS);
 
     private static final ExecutorService executorService = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("ResultLatch-%d").setDaemon(true).build());
 
-    private static final int SLEEP_TICKS = 100;
+    private static final int DEFAULT_POLL_MS = 250;
 
     private static final String STATUS_DONE = "done";
     private static final String STATUS_WAITING = "waiting";
     private static final String STATUS_RUNNING = "running";
 
+    /**
+     * @param client the client
+     * @param queryId the query
+     */
     public ResultLatch(QdsClient client, int queryId)
     {
         this(client, Integer.toString(queryId));
     }
 
+    /**
+     * @param client the client
+     * @param queryId the query
+     */
     public ResultLatch(QdsClient client, String queryId)
     {
         this.client = Preconditions.checkNotNull(client, "client cannot be null");
         this.queryId = Preconditions.checkNotNull(queryId, "queryId cannot be null").trim();
     }
 
+    /**
+     * Change the time that the query status is polled. The default
+     * is {@link #DEFAULT_POLL_MS}.
+     *
+     * @param time polling time
+     * @param unit time unit
+     */
+    public void setPollSleep(long time, TimeUnit unit)
+    {
+        pollMs.set(unit.toMillis(time));
+    }
+
+    /**
+     * Callback interface.
+     *
+     * @see ResultLatch#callback(Callback)
+     */
     public interface Callback
     {
+        /**
+         * Called when the result has been retrieved
+         *
+         * @param queryId the query
+         * @param resultValue the result
+         */
         public void result(String queryId, ResultValue resultValue);
 
+        /**
+         * Called if there was an error getting the result
+         *
+         * @param queryId the query
+         * @param e the error
+         */
         public void error(String queryId, Exception e);
     }
 
+    /**
+     * This method returns immediately and polls asynchronously for the result.
+     * When the result is retrieved (or there is an error) the callback is called
+     *
+     * @param callback callback
+     */
     public void callback(final Callback callback)
     {
         Runnable runnable = new Runnable()
@@ -59,11 +108,24 @@ public class ResultLatch
         executorService.submit(runnable);
     }
 
+    /**
+     * Await indefinitely for the result
+     *
+     * @throws Exception errors
+     */
     public void await() throws Exception
     {
         await(0, null);
     }
 
+    /**
+     * Await up to a maximum time for the result
+     *
+     * @param maxWait max wait time
+     * @param timeUnit time unit
+     * @return true if the result is ready, false if not or time has run out
+     * @throws Exception errors
+     */
     public boolean await(long maxWait, TimeUnit timeUnit) throws Exception
     {
         boolean hasWait = (timeUnit != null);
@@ -88,17 +150,31 @@ public class ResultLatch
                     return false;
                 }
             }
-            Thread.sleep(SLEEP_TICKS);
+            Thread.sleep(pollMs.get());
         }
         return true;
     }
 
+    /**
+     * Await indefinitely for the result
+     *
+     * @return the result
+     * @throws Exception errors
+     */
     public ResultValue awaitResult() throws Exception
     {
         await();
         return getResultValue();
     }
 
+    /**
+     * Await up to a maximum time for the result
+     *
+     * @param maxWait max wait time
+     * @param timeUnit time unit
+     * @return the result
+     * @throws Exception errors
+     */
     public ResultValue awaitResult(long maxWait, TimeUnit timeUnit) throws Exception
     {
         if ( !await(maxWait, timeUnit) )
--- src/test/java/com/qubole/qds/sdk/java/Tester.java
+++ src/test/java/com/qubole/qds/sdk/java/Tester.java
@@ -1,14 +1,12 @@
 package com.qubole.qds.sdk.java;
 
-import com.google.common.io.CharStreams;
 import com.qubole.qds.sdk.java.client.DefaultQdsConfiguration;
 import com.qubole.qds.sdk.java.client.QdsClient;
 import com.qubole.qds.sdk.java.client.QdsClientFactory;
 import com.qubole.qds.sdk.java.client.QdsConfiguration;
-import com.qubole.qds.sdk.java.client.ResultStreamer;
+import com.qubole.qds.sdk.java.client.ResultLatch;
+import com.qubole.qds.sdk.java.entities.CommandResponse;
 import com.qubole.qds.sdk.java.entities.ResultValue;
-import java.io.Reader;
-import java.util.Arrays;
 
 public class Tester
 {
@@ -19,25 +17,10 @@ public class Tester
         QdsClient client = QdsClientFactory.newClient(configuration);
         try
         {
-            ResultStreamer resultStreamer = new ResultStreamer(client);
-            ResultValue resultValue = new ResultValue();
-            resultValue.setInline(false);
-            resultValue.setResult_location(Arrays.asList("s3://dev.canopydata.com/logs/production/tmp/2014-05-20/1/1804089", "s3://dev.canopydata.com/logs/production/tmp/2014-05-20/1/1804089.dir/"));
-            Reader results = resultStreamer.getResults(resultValue);
-            for(;;)
-            {
-                int b = results.read();
-                if ( b < 0 )
-                {
-                    break;
-                }
-                System.out.print((char)(b & 0xffff));
-            }
-/*
-            Future<ResultValue> invoke = client.command().results("222070").inline(false).invoke();
-            ResultValue value = invoke.get();
-            System.out.println(value);
-*/
+            CommandResponse commandResponse = client.command().hive().query("show tables;").invoke().get();
+            ResultLatch resultLatch = new ResultLatch(client, commandResponse.getId());
+            ResultValue resultValue = resultLatch.awaitResult();
+            System.out.println(resultValue);
         }
         finally
         {
