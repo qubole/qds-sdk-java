b78ece1d5b0079b02da528b981ce4d0706124d43
Abhishek Srivastava <abhisheks@qubole.com>
2015-10-26 13:09:27 +0530

    SDK-48: Add support to clone a cluster (#48)
    New method to clone a cluster has been added to Cluster API.

--- examples/src/main/java/com/qubole/qds/sdk/java/examples/ClusterExample.java
+++ examples/src/main/java/com/qubole/qds/sdk/java/examples/ClusterExample.java
@@ -36,6 +36,10 @@ public class ClusterExample
         List<String> cluster_list = new ArrayList<String>();
         cluster_list.add(cluster_label);
 
+        String cluster_label_clone = "example_clone";
+        List<String> cluster_list_clone = new ArrayList<String>();
+        cluster_list_clone.add(cluster_label_clone);
+
         try {
             System.out.println("Creating new cluster...");
             ClusterItem clusterItem = client.cluster().create(client.cluster().clusterConfig()
@@ -55,9 +59,19 @@ public class ClusterExample
                     clusterItem.getCluster().getHadoop_settings().getMax_nodes());
             System.out.println();
 
-            System.out.println("Deleting cluster...");
+            System.out.println("Cloning cluster...");
+            ClusterItem clonedClusterItem = client.cluster().clone(cluster_label, client.cluster().clusterConfig()
+                    .label(cluster_list_clone)
+                    .enable_ganglia_monitoring(true)).invoke().get();
+            System.out.println("Cloned cluster id is: " +
+                    clonedClusterItem.getCluster().getId());
+            System.out.println();
+
+            System.out.println("Deleting clusters...");
             client.cluster().delete(cluster_label).invoke().get();
-            System.out.println("Cluster Deleted");
+            client.cluster().delete(String.valueOf(clonedClusterItem
+                .getCluster().getId())).invoke().get();
+            System.out.println("Clusters Deleted");
         }
         finally
         {
---
+++ src/test/java/com/qubole/qds/sdk/java/unittests/AbstractTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2014- Qubole Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qubole.qds.sdk.java.unittests;
+
+import java.util.Map;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import com.amazonaws.util.json.JSONObject;
+import com.google.common.base.Joiner;
+import com.qubole.qds.sdk.java.client.DefaultQdsConfiguration;
+import com.qubole.qds.sdk.java.client.QdsClient;
+import com.qubole.qds.sdk.java.client.QdsClientFactory;
+import com.qubole.qds.sdk.java.client.QdsConfiguration;
+import com.qubole.qds.sdk.java.details.InvokeArguments;
+
+public abstract class AbstractTest
+{
+    QdsConfiguration configuration;
+    QdsClient qdsClient;
+    ObjectMapper mapper;
+    
+    @BeforeClass
+    public void setup()
+    {
+        mapper = new ObjectMapper();
+        configuration = new DefaultQdsConfiguration("https://api.qubole.com/api","apiToken");
+        qdsClient = QdsClientFactory.newClient(configuration);
+    }
+    
+    public void assertRequestDetails(InvokeArguments<?> invokeargs, String expectedRequestType, String expectedEndpoint, JSONObject expectedRequestData, Map<Object, Object> expectedQueryParams, Class<?> expectedResponseClassType) throws Exception
+    {
+        Assert.assertEquals(invokeargs.getEntity().getMethod().name(),
+            expectedRequestType,
+            "Incorrect request type. Expected : "
+                + expectedRequestType
+                + " , got from request : "
+                + invokeargs.getEntity().getMethod().name());
+        Assert.assertEquals(Joiner.on("/").join(invokeargs.getAdditionalPaths()),
+            expectedEndpoint,
+            "Incorrect endpoint. Expected : "
+                + expectedEndpoint
+                + " , got from request : "
+                + Joiner.on("/").join(invokeargs.getAdditionalPaths()));
+        Assert.assertTrue(expectedResponseClassType.isInstance(invokeargs.getResponseType().newInstance()),
+            "Expected rsponse type was : " + expectedResponseClassType.getSimpleName()
+                + " , but it is of type : "
+                + invokeargs.getResponseType().getSimpleName());
+
+        if (expectedRequestData != null)
+        {
+            Assert.assertNotNull(invokeargs.getEntity().getEntity(),
+                "Request data is null. Expected was : " + expectedRequestData);
+            Assert.assertTrue(
+                mapper.readTree(invokeargs.getEntity().getEntity().toString())
+                    .equals(mapper.readTree(expectedRequestData.toString())),
+                "Incorrect request data. Expected was : " + expectedRequestData
+                    + " , got from request : " + invokeargs.getEntity().getEntity().toString());
+        }
+        else
+        {
+            Assert.assertNull(invokeargs.getEntity().getEntity(), "Request data expected was null. But it is : " + invokeargs.getEntity().getEntity().toString());
+        }
+        
+        if (expectedQueryParams != null)
+        {
+            Assert.assertNotNull(invokeargs.getEntity().getQueryParams(),
+                "Query params expected was not null. But it is null");
+            Assert.assertTrue(invokeargs.getEntity().getQueryParams().equals(expectedQueryParams));
+        }
+        else
+        {
+            Assert.assertNull(invokeargs.getEntity().getQueryParams(),
+                "Query params expected was null. But it is not null");
+        }
+    }
+}
