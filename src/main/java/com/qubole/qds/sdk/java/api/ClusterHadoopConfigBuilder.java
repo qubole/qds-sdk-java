2dc78510ebac059da6bfa566858a74c66e123bfd
Abhishek Srivastava <abhisheks@qubole.com>
2015-10-26 12:47:10 +0530

    SDK-47: Add support for creating haddop2 cluster

--- src/main/java/com/qubole/qds/sdk/java/api/ClusterHadoopConfigBuilder.java
+++ src/main/java/com/qubole/qds/sdk/java/api/ClusterHadoopConfigBuilder.java
@@ -22,6 +22,7 @@ public interface ClusterHadoopConfigBuilder
     public ClusterConfigBuilder max_nodes(int max_nodes);
     public ClusterConfigBuilder slave_instance_type(String slave_instance_type);
     public ClusterConfigBuilder slave_request_type(String slave_request_type);
+    public ClusterConfigBuilder use_hadoop2(boolean use_hadoop2);
     public ClusterConfigBuilder initial_nodes(int initial_nodes);
     public ClusterConfigBuilder custom_config(String custom_config);
     public ClusterSpotInstanceConfigBuilder spot_instance_settings();
